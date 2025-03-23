5c8d6b2b4e65279300f1cae402c12764c7df60c1
Trevor James Nangosha <trevornangosha16@gmail.com>
2025-03-23 18:26:46 +0300

    give the build another go!

--- app.config.js
+++ app.config.js
@@ -8,11 +8,6 @@ export default {
   icon: IS_PREVIEW ? "./assets/images/icon_thea_mobile_dev.png" : "./assets/images/icon_thea_mobile.png",
   scheme: "theamobile",
   userInterfaceStyle: "automatic",
-  // splash: {
-  //   image: IS_PREVIEW ? "./assets/images/splash_thea_mobile_dev.png" : "./assets/images/splash_thea_mobile.png",
-  //   resizeMode: "contain",
-  //   backgroundColor: "#ffffff",
-  // },
   ios: {
     supportsTablet: true,
   },
@@ -27,26 +22,35 @@ export default {
     ],
     package: IS_PREVIEW ? "com.bodastage.thea.preview" : "com.bodastage.thea",
   },
+  ios: {
+    "bundleIdentifier": IS_PREVIEW ? "com.bodastage.thea.preview" : "com.bodastage.thea",
+  },
   web: {
     bundler: "metro",
     output: "static",
     favicon: "./assets/images/favicon.png",
   },
   plugins: [
-    [
-      "expo-splash-screen",
-      {
-        // "autoHide": true,
-        splash: {
-          image: IS_PREVIEW ? "./assets/images/splash_thea_mobile_dev.png" : "./assets/images/splash_thea_mobile.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff"
-        }
-      }
-    ],
+    // SDK 52 has a tonne of issues/breaking changes which include(but are not limited to) issues 
+    // with the splash screen on Android 12+. It does not take a lot of work to find multiple issues on the expo github repo
+    // In the meantime, i will use the default screen provided by expo/android. It's not like a splash
+    // screen is a must have feature/core functionality anwyay.
+    // TODO; fix when the expo guys get their stuff in order
+    // [
+    //   "expo-splash-screen",
+    //   {
+    //     splash: {
+    //       image: IS_PREVIEW ? "./assets/images/icon_thea_mobile_dev.png" : "./assets/images/icon_thea_mobile.png",
+    //       imageWidth: 200,
+    //       resizeMode: "contain",
+    //       backgroundColor: "#ffffff"
+    //     }
+    //   }
+    // ],
+
+    
     "expo-router",
-    // "expo-background-task",
+    "expo-background-task",
     [
       "expo-location",
       {
